c3fbb34d6c118ab663cbe7fd70ec9b8dd63ee539
serafino <abraham.serafino@gmail.com>
2017-09-08 14:45:36 -0500

    Create client-side routes

---
+++ postcss.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  use: [
+    'assets',
+    'autoprefixer',
+    'cssnano',
+    'cssnext',
+    'focus',
+    'font-awesome',
+    'font-magician',
+    'write-svg',
+    'import',
+    'lost',
+    'css-mqpacker',
+    'normalize',
+    'precss',
+    'rucksack-css',
+    'simple-vars',
+    'sprites',
+    'stylelint'
+  ]
+};
--- src/client.js
+++ src/client.js
@@ -1,9 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import employeeList from './reducers/employeeList';
-import appState from './reducers/appState';
-import { createSimpleStore } from './reducers/SimpleStore';
 import io from 'socket.io-client';
 
 const socket = io();
@@ -11,10 +5,3 @@ const socket = io();
 socket.on('reconnect', () => {
   window.location.reload();
 });
-
-const store = createSimpleStore(employeeList, appState);
-
-ReactDOM.render(
-  <App store={store}/>,
-  document.getElementById('app')
-);
