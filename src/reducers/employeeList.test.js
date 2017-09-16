9230a3066535d81fc99ee27b4e936a207dc9b71a
serafino <abraham.serafino@gmail.com>
2017-09-16 10:29:39 -0500

    Removed PostCSS, as Sass should be sufficient for our purposes

--- src/reducers/employeeList.test.js
+++ src/reducers/employeeList.test.js
@@ -1,27 +1,29 @@
 import employeeList from './employeeList';
 
-test('Employee List - add employee', () => {
-  expect(new employeeList().addEmployee(
-      [{ name: 'Bob', rank: 'Manager', sn: 1 }],
+describe('Employee List', () => {
+  it('Employee List - add employee', () => {
+    expect(new employeeList().addEmployee(
+        [{ name: 'Bob', rank: 'Manager', sn: 1 }],
+        { name: 'Sally', rank: 'CEO', sn: 2 }
+    )).toEqual([
+      { name: 'Bob', rank: 'Manager', sn: 1 },
       { name: 'Sally', rank: 'CEO', sn: 2 }
-  )).toEqual([
-    { name: 'Bob', rank: 'Manager', sn: 1 },
-    { name: 'Sally', rank: 'CEO', sn: 2 }
-  ]);
-});
+    ]);
+  });
 
-test('Employee List - remove employee', () => {
-  const bob = { name: 'Bob', rank: 'Manager', sn: 1 };
-  const sally = { name: 'Sally', rank: 'CEO', sn: 2 };
+  it('Employee List - remove employee', () => {
+    const bob = { name: 'Bob', rank: 'Manager', sn: 1 };
+    const sally = { name: 'Sally', rank: 'CEO', sn: 2 };
 
-  expect(new employeeList().removeEmployee([bob, sally], bob))
+    expect(new employeeList().removeEmployee([bob, sally], bob))
     .toEqual([sally]);
-});
+  });
 
-test('Employee List - reset employees', () => {
-  const bob = { name: 'Bob', rank: 'Manager', sn: 1 };
-  const sally = { name: 'Sally', rank: 'CEO', sn: 2 };
+  it('Employee List - reset employees', () => {
+    const bob = { name: 'Bob', rank: 'Manager', sn: 1 };
+    const sally = { name: 'Sally', rank: 'CEO', sn: 2 };
 
-  expect(new employeeList().resetEmployees([], [bob, sally]))
+    expect(new employeeList().resetEmployees([], [bob, sally]))
     .toEqual([bob, sally]);
+  });
 });
